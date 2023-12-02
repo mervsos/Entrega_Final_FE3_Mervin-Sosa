079790e41c3efcfaea267eae0c587faa605cc4a3
mervsos <judcho8@gmail.com>
2023-12-02 13:12:22 +0100

    eliminación de archivos viejos

--- src/validate/validEmail.js
+++
@@ -1,5 +0,0 @@
-export const validEmail = email => {
-    const exReg = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    const res = exReg.test(email) ? true : false
-    return res
-}
--- src/validate/validName.js
+++
@@ -1,6 +0,0 @@
-export const validName = name => {
-    const exReg = /^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/
-    const mayorACinco = name.length > 5 ? true : false
-    const res = mayorACinco ? exReg.test(name.trim()) : false
-    return res
-}
